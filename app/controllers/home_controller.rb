0223f8cf3a9597b0d69a80cd9659e50e0fe23327
Park JunHyeong <wnsguddl789@gmail.com>
2017-12-03 20:17:39 +0900

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def myongjin_restaraunt
+
+  end
+  def officer_restaraunt
+
+  end
+
+  def student_restaraunt
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'home/myongjin_restaraunt'
+  get 'home/officer_restaraunt'
+  get 'home/student_restaraunt'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
