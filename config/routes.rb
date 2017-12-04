5414507733ce8f7a61b5bc005e62eb33ede3fdd0
JunHyeong Park <wnsguddl789@gmail.com>
2017-12-04 11:48:56 +0900

    move

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :models
   root 'home#index'
 
+  devise_scope :model do
+     get "/models/sign_out" => "devise/sessions#destroy"
+ end
+
   get 'home/myongjin_restaraunt'
   get 'home/officer_restaraunt'
   get 'home/student_restaraunt'
