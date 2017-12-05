e7f603b582aab208d4e3a6885665a29fd3a6593b
JunHyeong Park <wnsguddl789@gmail.com>
2017-12-05 17:08:28 +0900

    2017-12-05 2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
      get "/users/sign_out" => "devise/sessions#destroy"
  end
 
-  get 'home/myongjin_restaraunt'
-  get 'home/officer_restaraunt'
-  get 'home/student_restaraunt'
+  get 'home/myongjin'
+  get 'home/proffesor'
+  get 'home/student'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
