1107dbb0f79178175fbae334ff24399ec3bce5d5
a.mean.blogger <a.mean.blogger@gmail.com>
2019-12-27 11:37:13 -0800

    show, edit, update, destroy added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 // DB setting
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
 
 // DB schema
 var contactSchema = mongoose.Schema({
@@ -50,6 +52,34 @@ app.get('/contacts/new', function(req, res){
 // Contacts - create
 app.post('/contacts', function(req, res){
   Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
+// Contacts - show
+app.get('/contacts/:id', function(req, res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render('contacts/show', {contact:contact});
+  });
+});
+// Contacts - edit
+app.get('/contacts/:id/edit', function(req, res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render('contacts/edit', {contact:contact});
+  });
+});
+// Contacts - update
+app.put('/contacts/:id', function(req, res){
+  Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts/'+req.params.id);
+  });
+});
+// Contacts - destroy
+app.delete('/contacts/:id', function(req, res){
+  Contact.deleteOne({_id:req.params.id}, function(err){
     if(err) return res.json(err);
     res.redirect('/contacts');
   });
