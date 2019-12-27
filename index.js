b6b21108adc9e160e2e92ef24c2cda6ef1eb2890
a.mean.blogger <a.mean.blogger@gmail.com>
2019-12-27 11:36:08 -0800

    contact model added. index, new, create added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 
 // DB setting
@@ -19,6 +20,40 @@ db.on('error', function(err){
 // Other settings
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// DB schema
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phone:{type:String}
+});
+var Contact = mongoose.model('contact', contactSchema);
+
+// Routes
+// Home
+app.get('/', function(req, res){
+  res.redirect('/contacts');
+});
+// Contacts - Index
+app.get('/contacts', function(req, res){
+  Contact.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts});
+  });
+});
+// Contacts - New
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+// Contacts - create
+app.post('/contacts', function(req, res){
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
 
 // Port setting
 var port = 3000;
