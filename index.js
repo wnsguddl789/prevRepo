17a3d6845de7090a8539c2388fa0e2f88930ee5e
wnsguddl789 <wnsguddl789@gmail.com>
2021-01-28 14:46:06 +0900

    first commit

--- index.js
+++ index.js
@@ -25,65 +25,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'))
 
-// DB schema
-var contactSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
-  email:{type:String},
-  phone:{type:String}
-});
-var Contact = mongoose.model('contact', contactSchema);
-
 // Routes
-// Home
-app.get('/', function(req, res){
-  res.redirect('/contacts');
-});
-// Contacts - Index
-app.get('/contacts', function(req, res){
-  Contact.find({}, function(err, contacts){
-    if(err) return res.json(err);
-    res.render('contacts/index', {contacts:contacts});
-  });
-});
-// Contacts - New
-app.get('/contacts/new', function(req, res){
-  res.render('contacts/new');
-});
-// Contacts - create
-app.post('/contacts', function(req, res){
-  Contact.create(req.body, function(err, contact){
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
-// Contacts - show
-app.get('/contacts/:id', function(req, res){
-  Contact.findOne({_id:req.params.id}, function(err, contact){
-    if(err) return res.json(err);
-    res.render('contacts/show', {contact:contact});
-  });
-});
-// Contacts - edit
-app.get('/contacts/:id/edit', function(req, res){
-  Contact.findOne({_id:req.params.id}, function(err, contact){
-    if(err) return res.json(err);
-    res.render('contacts/edit', {contact:contact});
-  });
-});
-// Contacts - update
-app.put('/contacts/:id', function(req, res){
-  Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
-    if(err) return res.json(err);
-    res.redirect('/contacts/'+req.params.id);
-  });
-});
-// Contacts - destroy
-app.delete('/contacts/:id', function(req, res){
-  Contact.deleteOne({_id:req.params.id}, function(err){
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
+app.use('/', require('./routes/home'));
+app.use('/contacts', require('./routes/contacts'));
 
 // Port setting
 var port = 3000;
---
+++ routes/home.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+// Home
+router.get('/', function(req, res){
+  res.redirect('/contacts');
+});
+
+module.exports = router;
