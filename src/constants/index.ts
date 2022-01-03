77a761f9076bb4baeb100ffd31436f1e4defaede
HyunHo Lee <dlgusgh2001@naver.com>
2022-01-03 19:26:51 +0900

    🎉 쇼핑몰 프로젝트 초기 셋팅

---
+++ src/constants/index.ts
@@ -0,0 +1,31 @@
+// export const API_ENDPOINT = "";
+
+export const MEDIA_QUERY_END_POINT = {
+  MOBILE: "640px",
+  TABLET: "1280px",
+  DESKTOP: "1920px",
+  LARGE: "2560px",
+};
+
+// export const ROUTES = [
+//   {
+//     ID: 0,
+//     PATH: "/",
+//     LABEL: "Home",
+//   },
+//   {
+//     ID: 1,
+//     PATH: "/recent",
+//     LABEL: "Recent",
+//   },
+//   {
+//     ID: 2,
+//     PATH: "/search",
+//     LABEL: "Search",
+//   },
+// ];
+
+// CSS
+// export const PALLETS = {
+//   MAIN: "#6868AD",
+// };
