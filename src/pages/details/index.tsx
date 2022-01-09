c495a265346ea40de60c4bd31c08693a55da49bb
HyunHo Lee <dlgusgh2001@naver.com>
2022-01-09 15:50:07 +0900

    💄상세 페이지 헤더 UI

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,20 +1,26 @@
 import type { NextPage } from 'next';
 import styled from '@emotion/styled';
+import { DetailsHeader } from '../../components/Details/DetailsHeader';
 
 const DetailsIndexPage: NextPage = () => {
   return (
-    <section>
+    <Container>
+      <article>
+        <h2 className="sr-only">하트링 상세 페이지</h2>
+        <img
+          src="https://cdn.pixabay.com/photo/2015/11/14/14/42/heart-1043245_960_720.jpg"
+          alt=""
+        />
+        <div>skldfjslkdfjsdlkf</div>
+      </article>
+      <DetailsHeader />
       <h2 className="sr-only">상세 페이지</h2>
-      <header>
-        <ul>
-          <li>홈</li>
-          <li>반지</li>
-          <li>미스링</li>
-          <li>물고기 반지</li>
-        </ul>
-      </header>
-    </section>
+    </Container>
   );
 };
 // rgba(0,0,0,0.5);
 export default DetailsIndexPage;
+
+const Container = styled.section`
+  padding: 0 150px;
+`;
