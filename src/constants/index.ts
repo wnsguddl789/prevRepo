3fe0079965f2b82aef10ccc845573e9ab58f921c
BBAK_JUN <wnsguddl789@gmail.com>
2022-01-27 09:30:22 +0900

    ✨상품 디테일페이지 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
 // export const API_ENDPOINT = "";
 
 export const MEDIA_QUERY_END_POINT = {
-  MOBILE: "640px",
-  TABLET: "1280px",
-  DESKTOP: "1920px",
-  LARGE: "2560px",
+  MOBILE: '640px',
+  TABLET: '1280px',
+  DESKTOP: '1920px',
+  LARGE: '2560px',
 };
 
 // export const ROUTES = [
@@ -29,3 +29,18 @@ export const MEDIA_QUERY_END_POINT = {
 // export const PALLETS = {
 //   MAIN: "#6868AD",
 // };
+
+export const DUMMY_DATA = {
+  title: '이고진 스미스머신 3022SM',
+  description: '전신 웨이트 운동, 그 어떤 운동기구보다 안전하고 완벽하게',
+  price: 1722400,
+  discount_percent: 20,
+  imageArray: [
+    'https://egojin.com/web/product/small/202111/801dbda7ed61c189babef276c4d3272f.jpg',
+    'https://egojin.com/web/product/extra/small/202111/82731e1917a7d7acc0433360318e646b.jpg',
+    'https://egojin.com/web/product/extra/small/202111/e24bbf72e6feea2f9c985c7ce0903749.jpg',
+    'https://egojin.com/web/product/extra/small/202111/c01457039ae84f62519cb38aa33f4c21.jpg',
+    'https://egojin.com/web/product/extra/small/202111/0bdfb00fdc571b43ee925cb0d7398fd0.jpg',
+  ],
+  likes: 500,
+};
