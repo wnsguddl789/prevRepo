e924aa5c26141383dab50e7df80cbcfe1edf44f2
BBAK_JUN <wnsguddl789@gmail.com>
2022-01-28 15:19:11 +0900

    :sparkles: ClickToEdit 컴포넌트 개발완료

--- src/component/Tag.js
+++ src/component/Tag.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Tag = () => {};
+const Tag = () => {
+  return (
+    <Container>
+      <Title>Tag</Title>
+      <TagContainer></TagContainer>
+    </Container>
+  );
+};
 
 export default Tag;
+
+const Container = styled.div``;
+const Title = styled.p``;
+const TagContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 50px 0;
+`;
