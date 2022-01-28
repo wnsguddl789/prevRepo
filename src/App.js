5815696936378fce41dbfd69059bddb9e977ee5b
BBAK_JUN <wnsguddl789@gmail.com>
2022-01-28 10:36:03 +0900

    ✨토글 컴포넌트 개발 안료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
-}
+import Toggle from './component/Toggle';
+import styled from 'styled-components';
+const App = () => {
+  return (
+    <AppContainer>
+      <Toggle />
+    </AppContainer>
+  );
+};
 
 export default App;
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
