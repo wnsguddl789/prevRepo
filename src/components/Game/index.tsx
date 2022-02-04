fde67439fe64e9404df7b56e63a2a02aed4c15de
BBAK_JUN <wnsguddl789@gmail.com>
2022-02-04 14:36:47 +0900

    🚑️ 랜덤 범위 재설정

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -2,17 +2,15 @@ import { useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { COLOR } from '../../constant/index';
 interface GameProps {
-  score: number;
-  stage: number;
   NUMBER: number;
   handleCorrect: Function;
   handleWrong: Function;
 }
-const Game = ({ score, NUMBER, stage, handleCorrect, handleWrong }: GameProps) => {
+const Game = ({ NUMBER, handleCorrect, handleWrong }: GameProps) => {
   // N = stage + 1 -> N X N 크기의 배열생성
   const [array, setArray] = useState(Array.from(Array(NUMBER * NUMBER).keys()));
   // 0 ~ stage 까지의 랜덤상수 생성
-  const [randomInt, setRandomInt] = useState(Math.floor(Math.random() * (stage - 0)) + 0);
+  const [randomInt, setRandomInt] = useState(Math.floor(Math.random() * (NUMBER - 1)) + 0);
   const [colorCode, setColorCode] = useState([
     `#${Math.round(Math.random() * 0xffffff).toString(16)}`,
     `#${Math.round(Math.random() * 0xffffff).toString(16)}`,
@@ -22,12 +20,8 @@ const Game = ({ score, NUMBER, stage, handleCorrect, handleWrong }: GameProps) =
     if (ref.current) {
       let repeat = Array(NUMBER + 1).join('1fr ');
       ref.current.setAttribute('style', `grid-template-columns : ${repeat}`);
-      [...ref.current.children].map((child) => {
-        // console.log(child);
-      });
     }
-    // console.log(array);
-  }, [ref, stage]);
+  }, [NUMBER, ref]);
   const Correct = () => {
     setArray(Array.from(Array((NUMBER + 1) * (NUMBER + 1)).keys()));
     setRandomInt(Math.floor(Math.random() * (NUMBER - 0)) + 0);
@@ -39,9 +33,9 @@ const Game = ({ score, NUMBER, stage, handleCorrect, handleWrong }: GameProps) =
       {array &&
         array.map((list, idx) => {
           if (idx === randomInt) {
-            return <Card key={idx} index={String(stage)} isCorrect={true} onClick={() => Correct()} />;
+            return <Card key={idx} index={String(NUMBER - 1)} isCorrect={true} onClick={() => Correct()} />;
           } else {
-            return <Card key={idx} index={String(stage)} isCorrect={false} onClick={() => handleWrong()} />;
+            return <Card key={idx} index={String(NUMBER - 1)} isCorrect={false} onClick={() => handleWrong()} />;
           }
         })}
     </Container>
