56af09d1963d35fedcbb888f2df5d4edc49d030b
BBAK_JUN <wnsguddl789@gmail.com>
2022-02-08 11:26:43 +0900

    ✨메세지 컴포넌트 개발완료

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface PropsTypes {
+  text: string;
+  color: string;
+}
+
+const Message = ({ text, color }: PropsTypes) => (
+  <Container>
+    <Text color={color}>{text}</Text>
+  </Container>
+);
+
+export default Message;
+const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+const Text = styled.span`
+  color: ${(props) => props.color};
+`;
