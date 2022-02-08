932f142ceed8de537f8034820dba3368beee3882
BBAK_JUN <wnsguddl789@gmail.com>
2022-02-08 22:25:33 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,18 +23,18 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <>
       <HelmetProvider>
         <Helmet>
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://example.com/page.html" />
-          <meta property="og:title" content="Content Title" />
-          <meta
-            property="og:image"
-            content="https://media.vlpt.us/images/wnsguddl789/post/e78f377c-d471-4780-9246-10e8eb755de4/image%20(1).png"
-          />
-          <meta property="og:description" content="Description Here" />
-          <meta property="og:site_name" content="Site Name" />
-          <meta property="og:locale" content="en_US" />
           <title>Nomflix{title}</title>
         </Helmet>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://example.com/page.html" />
+        <meta property="og:title" content="Content Title" />
+        <meta
+          property="og:image"
+          content="https://media.vlpt.us/images/wnsguddl789/post/e78f377c-d471-4780-9246-10e8eb755de4/image%20(1).png"
+        />
+        <meta property="og:description" content="Description Here" />
+        <meta property="og:site_name" content="Site Name" />
+        <meta property="og:locale" content="en_US" />
       </HelmetProvider>
       <AppLayout>
         <Component {...pageProps} />
