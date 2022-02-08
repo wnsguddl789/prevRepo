739cbd15cc96fe596d4b1008c74281208b553d16
BBAK_JUN <wnsguddl789@gmail.com>
2022-02-08 22:30:42 +0900

    Head 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import '../../styles/reset.css';
 import AppLayout from '../components/AppLayout';
 import { ROUTES } from '../constant';
 import { GetServerSideProps } from 'next';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [title, setTitle] = useState('');
@@ -25,6 +26,18 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <title>Nomflix{title}</title>
         </Helmet>
       </HelmetProvider>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://example.com/page.html" />
+        <meta property="og:title" content="Content Title" />
+        <meta
+          property="og:image"
+          content="https://media.vlpt.us/images/wnsguddl789/post/e78f377c-d471-4780-9246-10e8eb755de4/image%20(1).png"
+        />
+        <meta property="og:description" content="Description Here" />
+        <meta property="og:site_name" content="Site Name" />
+        <meta property="og:locale" content="en_US" />
+      </Head>
       <AppLayout>
         <Component {...pageProps} />
       </AppLayout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,18 +7,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://example.com/page.html" />
-          <meta property="og:title" content="Content Title" />
-          <meta
-            property="og:image"
-            content="https://media.vlpt.us/images/wnsguddl789/post/e78f377c-d471-4780-9246-10e8eb755de4/image%20(1).png"
-          />
-          <meta property="og:description" content="Description Here" />
-          <meta property="og:site_name" content="Site Name" />
-          <meta property="og:locale" content="en_US" />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
