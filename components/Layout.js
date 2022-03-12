5a04efb2ee65320938a060b8f8d64344c2e2323c
BBAK_JUN <wnsguddl789@gmail.com>
2022-03-12 23:12:50 +0900

    ✨장바구니 뱃지 상태 전역관리

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
-import { AppBar, Toolbar, Typography, Container, Link, createTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Container, Link, createTheme, ThemeProvider, CssBaseline, Badge } from '@material-ui/core';
 import useStyles from '../utils/styles';
 import { Store } from '../utils/store';
 import { Switch } from '@mui/material';
@@ -9,7 +9,7 @@ import Cookies from 'js-cookie';
 
 function Layout({ title, description, children }) {
   const { state, dispatch } = useContext(Store);
-  const { darkMode } = state;
+  const { darkMode, cart } = state;
   const theme = createTheme({
     typography: {
       h1: {
@@ -59,7 +59,15 @@ function Layout({ title, description, children }) {
             <div>
               <Switch checked={darkMode} onChange={darkModeChangeHandler}></Switch>
               <NextLink href="/cart" passHref>
-                <Link>Cart</Link>
+                <Link>
+                  {cart.cartItems.length > 0 ? (
+                    <Badge color="secondary" badgeContent={cart.cartItems.length}>
+                      Cart
+                    </Badge>
+                  ) : (
+                    'Cart'
+                  )}
+                </Link>
               </NextLink>
               <NextLink href="/login" passHref>
                 <Link>Login</Link>
