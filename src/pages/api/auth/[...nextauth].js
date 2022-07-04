bfadcb8e4341e78573a233fe6d2618a0b0a9af60
BBAK_JUN <wnsguddl789@gmail.com>
2022-07-04 22:58:12 +0900

    fix : build 타입 오류 변경

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,47 +4,52 @@ import KakaoProvider from "next-auth/providers/kakao";
 import NaverProvider from "next-auth/providers/naver";
 
 export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
-    }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID,
-      clientSecret: process.env.NAVER_CLIENT_SECRET,
-    }),
-  ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, user, token }) {
-      session = {
-        ...session,
-        user: {
-          ...session.user,
-          id: token.sub,
-        },
-        accessToken: token.accessToken,
-      };
-      return session;
-    },
-    async signIn({ user, account, profile, email, credentials }) {
-      return true;
-    },
-  },
-  pages: {
-    signIn: "/auth/signin",
-  },
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET
+		}),
+		KakaoProvider({
+			clientId: process.env.KAKAO_CLIENT_ID,
+			clientSecret: process.env.KAKAO_CLIENT_SECRET
+		}),
+		NaverProvider({
+			clientId: process.env.NAVER_CLIENT_ID,
+			clientSecret: process.env.NAVER_CLIENT_SECRET
+		})
+	],
+	secret: process.env.SECRET,
+	theme: {
+		colorScheme: "light"
+	},
+	session: {
+		strategy: "jwt",
+		maxAge: 30 * 24 * 60 * 60 // 30 days
+	},
+	callbacks: {
+		async jwt({ token, account }) {
+			// Persist the OAuth access_token to the token right after signin
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, user, token }) {
+			session = {
+				...session,
+				user: {
+					...session.user,
+					id: token.sub
+				},
+				accessToken: token.accessToken
+			};
+			return session;
+		},
+		async signIn({ user, account, profile, email, credentials }) {
+			return true;
+		}
+	},
+	pages: {
+		signIn: "/auth/signin"
+	}
 });
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,30 +1,3 @@
 import socketIOClient, { Socket } from "socket.io-client";
-import { addGameType } from "../types/game";
 
-export interface ServerToClientEvents {
-	noArg: () => void;
-	basicEmit: (a: number, b: string, c: Buffer) => void;
-	withAck: (d: string, callback: (e: number) => void) => void;
-	addGameRoom: (gameRoom: any) => void;
-	drawing: (onDrawingEvent: any) => void;
-	enterGameRoom: (user: any) => void;
-	addChat: (chatData: any) => void;
-	nextStep: (step: number) => void;
-	setKeyword: (keyword: any) => void;
-}
-
-interface ClientToServerEvents {
-	hello: () => void;
-}
-
-interface InterServerEvents {
-	ping: () => void;
-}
-
-interface SocketData {
-	name: string;
-	age: number;
-}
-
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> =
-	socketIOClient("http://localhost:4000");
+export const socket = socketIOClient("http://localhost:4000");
